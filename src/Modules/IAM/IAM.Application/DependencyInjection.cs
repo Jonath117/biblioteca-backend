4f5fc3044f165d35bdbeb4d4895433700c83c23e
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-13 17:17:37 -0400

    se registraron los servicios de Interfaces como Usuario para repository y del generador de tokens

--- src/Modules/IAM/IAM.Application/DependencyInjection.cs
+++ src/Modules/IAM/IAM.Application/DependencyInjection.cs
@@ -1,6 +1,16 @@
+using IAM.Application.Interfaces.Authentication;
+using IAM.Application.Interfaces.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace IAM.Application;
 
-public class DependencyInjection
+public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddIamApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IUsuarioRepository>();
+        services.AddScoped<IJwtTokenGenerator>();
+        
+        return services;
+    }
 }

1d0636c984c4946caea116d0a3c3c738e9105cc4
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-14 00:34:25 -0400

    se registraron los servicios de IamApplication

--- src/Core/Web.API/Program.cs
+++ src/Core/Web.API/Program.cs
@@ -1,4 +1,5 @@
 using Catalog.Infrastructure;
+using IAM.Application;
 using IAM.Infrastructure;
 using Workflow.Infrastructure;
 using Workspace.Infrastructure;
@@ -6,6 +7,8 @@ using Workspace.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddIamInfrastructure(builder.Configuration);
+builder.Services.AddIamApplication();
+
 builder.Services.AddWorkspaceInfrastructure(builder.Configuration);
 builder.Services.AddWorkflowInfrastructure(builder.Configuration);
 builder.Services.AddCatalogInfrastructure(builder.Configuration);

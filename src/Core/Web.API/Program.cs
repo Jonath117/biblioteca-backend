738c78a3dc7c9d89086dbfcfb8f5db4842f82f44
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-15 15:40:05 -0400

    se agrego permisos para el CORS para que se puedan hacer llamadas desde el frontend

--- src/Core/Web.API/Program.cs
+++ src/Core/Web.API/Program.cs
@@ -1,6 +1,7 @@
 using Catalog.Infrastructure;
 using IAM.Application;
 using IAM.Infrastructure;
+using IAM.Presentation;
 using Workflow.Infrastructure;
 using Workspace.Infrastructure;
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddIamInfrastructure(builder.Configuration);
 builder.Services.AddIamApplication();
+builder.Services.AddIamPresentation();
 
 builder.Services.AddWorkspaceInfrastructure(builder.Configuration);
 builder.Services.AddWorkflowInfrastructure(builder.Configuration);
@@ -17,7 +19,31 @@ builder.Services.AddCatalogInfrastructure(builder.Configuration);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("FrontendPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173") 
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials(); 
+    });
+});
+
+builder.Services.AddEndpointsApiExplorer();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerUI(options => 
+    {
+        // Define la ruta del endpoint OpenAPI
+        options.SwaggerEndpoint("/openapi/v1.json", "Mi API v1");
+    });
+}
+
+app.UseCors("FrontendPolicy");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -25,6 +51,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();

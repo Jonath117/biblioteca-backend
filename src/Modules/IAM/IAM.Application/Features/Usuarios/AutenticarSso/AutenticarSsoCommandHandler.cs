416ca39cc863d9879694efc7d1b1d945e7d891a3
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-06-13 18:29:22 -0400

    se modifico el namespace y tambien el metodo de creacion de acuerdo al tipo de login

--- src/Modules/IAM/IAM.Application/Features/Usuarios/AutenticarSso/AutenticarSsoCommandHandler.cs
+++ src/Modules/IAM/IAM.Application/Features/Usuarios/AutenticarSso/AutenticarSsoCommandHandler.cs
@@ -5,7 +5,7 @@ using IAM.Domain.Entities;
 using IAM.Domain.Exceptions;
 using MediatR;
 
-namespace IAM.Application.Features.Usuarios.LoginConGoogle;
+namespace IAM.Application.Features.Usuarios.AutenticarSso;
 
 public class AutenticarSsoCommandHandler : IRequestHandler<AutenticarSsoCommand, string>
 {

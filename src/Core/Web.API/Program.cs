0fe5b8073760ec3957b6354b5f930c8c652a0739
Matias Palacios <molinapalaciosjosuematias@gmail.com>
2026-06-17 00:59:21 -0400

    update

--- src/Core/Web.API/Program.cs
+++ src/Core/Web.API/Program.cs
@@ -1,32 +1,65 @@
 using Catalog.Infrastructure;
+using IAM.Application;
 using IAM.Infrastructure;
+using IAM.Presentation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Workflow.Application;
 using Workflow.Infrastructure;
+using Workflow.Presentation;
 using Workspace.Application;
 using Workspace.Infrastructure;
+using Workspace.Presentation;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddIamInfrastructure(builder.Configuration);
-builder.Services.AddWorkspaceInfrastructure(builder.Configuration);
-builder.Services.AddWorkspaceApplication();
-builder.Services.AddWorkflowInfrastructure(builder.Configuration);
-builder.Services.AddCatalogInfrastructure(builder.Configuration);
-
-// Add services to the container.
 builder.Services.AddControllers();
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+            ValidAudience = builder.Configuration["JwtSettings:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Secret"]!))
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddIamInfrastructure(builder.Configuration);
+builder.Services.AddIamApplication();
+builder.Services.AddIamPresentation();
+
+builder.Services.AddWorkflowApplication();
+builder.Services.AddWorkflowInfrastructure(builder.Configuration);
+builder.Services.AddWorkflowPresentation();
+
+builder.Services.AddWorkspaceInfrastructure(builder.Configuration);
+builder.Services.AddWorkspaceApplication();
+builder.Services.AddWorkspacePresentation();
+
+builder.Services.AddCatalogInfrastructure(builder.Configuration);
+
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
